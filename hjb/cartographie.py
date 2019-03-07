3baabe55551ae08c2d57671632fa3d0a719112cb
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2019-03-07 16:31:00 +0100

    Creation cartographie.

---
+++ hjb/cartographie.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 perrollaz <perrollaz@vincent-MacBookPro>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Cartographie de la fonction valeur.
+"""
